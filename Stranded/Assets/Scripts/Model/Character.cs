7dfd368dc1264cd76c43821e3d3cfc87aa60dc23
Andrew Huang <A.S.Huang@student.tudelft.nl>
2021-11-30 20:53:54 +0100

    Add Character dataclass

---
+++ Stranded/Assets/Scripts/Model/Character.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Model
+{    
+    public enum Gender 
+    {
+        MALE,
+        FEMALE
+    }
+
+    public enum Race
+    {
+        WHITE,
+        BLACK,
+        MIDDLE_EASTERN,
+        ASIAN,
+        HISPANIC
+    }
+
+    public enum Personality
+    {
+        QUIRKY,
+        HASTY,
+        EXTROVERT,
+        INTROVERT
+    }
+
+    public class Character : MonoBehaviour, IEquatable<Character>
+    {
+
+        public string m_name;
+        public int m_age;
+        public string m_description;
+
+        // TODO find better ways to utilize these attributes
+        public Gender m_gender;
+        public Race m_race;
+        public Personality m_personality;
+
+        // Uncomment when merging with Attribute
+        // public List<Attributes> m_proficiency;
+
+        private int m_id;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        public void SetId(int id) 
+        {
+            m_id = id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Character);
+        }
+
+        public override int GetHashCode() {
+            return m_id;
+        }
+
+        public bool Equals(Character other) 
+        {
+            if (other == null)
+                return false;
+            return m_id == other.m_id;
+        }
+        
+
+    }
+}
