6d512cf848a2f4f012d8874abcd705f3c5aa8b07
Siert <s.j.g.sebus@student.tudelft.nl>
2021-12-08 20:43:20 +0100

    Implement the card grid

---
+++ Stranded/Assets/Scripts/UI/GridLayoutGroupAutoScaler.cs
@@ -0,0 +1,34 @@
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+  public class GridLayoutGroupAutoScaler : MonoBehaviour
+  {
+    public GridLayoutGroup gridLayoutGroup;
+
+    public RectTransform container;
+    
+    public int numberOfColumns;
+
+    [Tooltip("Width divided by height.")]
+    public float cardAspectRatio;
+    
+    private void Awake()
+    {
+      var cardWidth = container.rect.width / numberOfColumns;
+      var cardHeight = cardWidth / cardAspectRatio;
+      var noOfCards = container.childCount;
+      var noOfRows = (int) math.ceil((float) noOfCards / numberOfColumns);
+
+      
+      var containerHeightResize = cardHeight * noOfRows - container.rect.height;
+      container.sizeDelta = new Vector2(0, containerHeightResize);
+      container.anchoredPosition = new Vector2(0, -containerHeightResize / 2);
+      
+      gridLayoutGroup.cellSize = new Vector2(cardWidth, cardHeight); 
+    }
+  }
+}
+
