16a5af0679c9a54889f87f5290ca08148a94b7c0
Siert <s.j.g.sebus@student.tudelft.nl>
2021-12-09 08:16:02 +0100

    Create the main UI components for the job scroller.

---
+++ Stranded/Assets/Scripts/UI/JobScrollerSelector.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace UI
+{
+  public class JobScrollerSelector : MonoBehaviour
+  {
+    public RectTransform selectedJobFrame;
+
+    public RectTransform scrollContent;
+    
+    
+  }
+}
