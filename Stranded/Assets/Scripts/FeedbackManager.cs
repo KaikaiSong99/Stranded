60d99ad108a7ea4e6e60792de822b01e30c70bcd
Siert <s.j.g.sebus@student.tudelft.nl>
2021-12-09 15:40:43 +0100

    Fix layout issues.

--- Stranded/Assets/Scripts/FeedbackManager.cs
+++ Stranded/Assets/Scripts/FeedbackManager.cs
@@ -11,29 +11,29 @@ public class FeedbackManager : MonoBehaviour
     public GameObject feedbackPrefab;
     public VerticalLayoutGroup feedbackVGroup;
 
-    private List<GameObject> feedbackObjects;
+    private List<GameObject> feedbackObjects = new List<GameObject>();
 
     public void Start()
     {
-        feedbackObjects = new List<GameObject>();
     }
 
     public IEnumerator ShowFeedback(int roundScore, List<Character> characters)
     {
         score.text = String.Format("Your score for this round: {0}", roundScore);
-
+        
         foreach (var character in characters) 
         {
-            var gameObject = Instantiate<GameObject>(feedbackPrefab);
+            var gameObject = Instantiate<GameObject>(feedbackPrefab, transform);
             gameObject.transform.SetParent(feedbackVGroup.transform);
-
+        
             FeedbackStats feedbackStats = gameObject.GetComponent<FeedbackStats>();
             feedbackStats.Show(character);
-
+            Debug.Log(feedbackObjects);
             feedbackObjects.Add(gameObject);
-            yield return new WaitForSeconds(3);
-
+            
+            yield return new WaitForSeconds(3f);
         }
+    
         yield return StartCoroutine(Cleanup());
     }
 
