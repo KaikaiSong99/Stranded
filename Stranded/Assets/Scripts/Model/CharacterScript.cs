1247ff8f9fd8d71bb895570b497bb1acd7abbcba
Andrew Huang <A.S.Huang@student.tudelft.nl>
2022-01-18 18:39:17 +0100

    Make Assignment functional

---
+++ Stranded/Assets/Scripts/Model/CharacterScript.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+using Model; 
+
+public class CharacterScript : MonoBehaviour, IPointerClickHandler
+{
+    public Image portrait;
+    public TextMeshProUGUI name;
+    public TextMeshProUGUI age;
+    public TextMeshProUGUI description;
+    public Image assignedJob;
+
+    public Character _character;
+    public Character Character
+    {
+        get => _character;
+        set { 
+            _character = value; 
+            name.text = value.name;
+            portrait.sprite = value.portrait;
+            age.text = value.age.ToString();
+            description.text = value.description;
+        }
+    }
+
+    public static event Action<Character> onCharacterPressed;
+
+    public void SetPickedJob(Job job)
+    {
+        if (job)
+        {
+            assignedJob.sprite = job.jobIcon;
+        }
+    }
+
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+        onCharacterPressed?.Invoke(_character);
+    }
+
+
+
+
+}
